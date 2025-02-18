32ba379bffa518b1a96e7228a6656d1ed9ae5cae
Vivek Katkar <102805058+vivekkatkar@users.noreply.github.com>
2025-02-18 22:27:49 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,18 +1,49 @@
-const express = require("express")
+require("dotenv").config();
+const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+
 const app = express();
 app.use(express.json());
+app.use(cors({ origin: "*" }));
 
-app.use(cors({
-    origin : "*"
-}));
+console.log("MONGO_HOST:", process.env.MONGO_HOST);
+console.log("MONGO_PORT:", process.env.MONGO_PORT);
+console.log("MONGO_DB:", process.env.MONGO_DB);
+
+const MONGO_URI = `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`;
+
+
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log("Connected to MongoDB"))
+.catch((err) => console.error("MongoDB connection error:", err));
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    age: Number
+});
+const User = mongoose.model("User", userSchema);
 
 app.get("/get-text", (req, res) => {
-    const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Curabitur pretium tincidunt lacus. Nulla gravida orci a odio. Nullam varius, turpis et commodo pharetra, est eros bibendum elit, nec luctus magna felis sollicitudin mauris.";
-
+    const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
     res.send(text);
 });
 
-app.listen(3000, ()=>{
-    console.log(`app is running on port ${3000}`);
+app.get("/get-users", async (req, res) => {
+    try {
+        const users = await User.find(); 
+        res.json(users);
+    } catch (error) {
+        console.error("Error fetching users:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`App is running on port ${PORT}`);
 });
